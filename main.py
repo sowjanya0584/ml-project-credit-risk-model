1f8df3b685428db120f64f336c840875b3463e92
Kalyanapu <kalyanapu@kalyanapusmbp2.myfiosgateway.com>
2025-03-06 11:50:47 -0500

    added files

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from prediction_helper import predict  # Ensure this is correctly linked to your prediction_helper.py
-
+from prediction_helper import predict
 # Set the page configuration and title
 st.set_page_config(page_title="Lauki Finance: Credit Risk Modelling", page_icon="📊")
 st.title("Lauki Finance: Credit Risk Modelling")
